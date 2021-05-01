87e113959e9060b3fbc3311484c97926b88551d4
kimjanghu <kjh921030@gmail.com>
2021-05-02 00:08:28 +0900

    refactor: Edit props

--- github-issue/src/components/Navbar/Navbar.jsx
+++ github-issue/src/components/Navbar/Navbar.jsx
@@ -11,20 +11,21 @@ const NEW_MILESTONE = "New milestone"
 
 const Navbar = ({ type, setType, newTypeFlag, setNewTypeFlag }) => {
   const selectedNewType = type === NAV_MANU.LABELS ? NEW_LABEL : NEW_MILESTONE
+  const renderTypeButton = ({ typeName, props }) => {
+    return typeName.map(name => <TypeButton key={name} name={name} typeFlag={type===name} {...props}></TypeButton>)
+  }
+  
   return (
     <Navigation>
       <Wrapper newTypeFlag={newTypeFlag}>
         <Type>
-          <TypeButton 
-            name={NAV_MANU.LABELS} 
-            typeFlag={type===NAV_MANU.LABELS}
-            setType={setType}
-          />
-          <TypeButton 
-            name={NAV_MANU.MILESTONES} 
-            typeFlag={type===NAV_MANU.MILESTONES} 
-            setType={setType} 
-          />
+          {
+            renderTypeButton({
+              typeName: [NAV_MANU.LABELS, NAV_MANU.MILESTONES],
+              props: { type, setType, setNewTypeFlag }
+            })
+          }
+          
         </Type>
         <div>
           <OperationButton 
@@ -38,6 +39,7 @@ const Navbar = ({ type, setType, newTypeFlag, setNewTypeFlag }) => {
     </Navigation>
   )
 }
+
 const Navigation = styled.div`
   margin: 32px 0;
 `
