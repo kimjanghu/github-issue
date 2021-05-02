06f4a1ed036e50c5daabfd1158fde60cce676f05
kimjanghu <kjh921030@gmail.com>
2021-05-02 09:46:43 +0900

    style: Split styled component

--- github-issue/src/components/Milestones/MilestonesFormSection.jsx
+++ github-issue/src/components/Milestones/MilestonesFormSection.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useContext } from "react";
-import styled from "styled-components";
 import { postMilestones } from "../../api/api"
 import { MilestonesContext } from "../../Context/Context"
 import { BUTTON_NAME } from "../../utils/constants"
 import OperateButton from "../Buttons/OperateButton"
 import SubmitButton from "../Buttons/SubmitButton"
+import { 
+  FormSection, 
+  Form, 
+  LabelWrapper, 
+  FormOperationWrapper 
+} from "./MilestonesFormSection.style"
 
 const TITLE = "Title"
 const DUE_DATE = "Due date"
@@ -96,42 +101,5 @@ const MilestonesFormSection = ({ newTypeFlag, setNewTypeFlag }) => {
     </>
   )
 }
-
-const FormSection = styled.form`
-
-`
-
-const Form = styled.div`
-  padding: 16px 0;
-  border-top: 1px solid #dbdee2;
-  border-bottom: 1px solid #dbdee2;
-`
-
-const LabelWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  label {
-    font-weight: bold;
-    margin: 8px 0;
-  }
-
-  input {
-    width: 500px;
-    padding: 8px;
-    font-size: 20px;
-  }
-
-  textarea {
-    width: 600px;
-    height: 300px;
-    font-size: 20px;
-  }
-`
-
-const FormOperationWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`
 
 export default MilestonesFormSection;
